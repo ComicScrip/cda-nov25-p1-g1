147c623f02b2a2c04bbfe8d4400794a446f679c9
on4r4p <on4r4p@disrot.org>
2025-12-22 12:04:23 +0100

    Adapt resetdb to MLD and edit User entities

--- backend/src/db/reset.ts
+++ backend/src/db/reset.ts
@@ -16,9 +16,23 @@ async function removeDBFile() {
 async function createSchema() {
   await db.initialize();
   await db.query("PRAGMA foreign_keys = ON");
-  await db.query("DROP TABLE IF EXISTS user");
+  await db.query("DROP TABLE IF EXISTS Game");
+  await db.query("DROP TABLE IF EXISTS Attempt");
+  await db.query("DROP TABLE IF EXISTS Word");
+  await db.query("DROP TABLE IF EXISTS User_");
 
   const schemaStatements = [
+    `CREATE TABLE IF NOT EXISTS User_ (
+      id_user INTEGER PRIMARY KEY,
+      username VARCHAR(50),
+      role VARCHAR(50),
+      password VARCHAR(50),
+      creation_date DATE,
+      games_played VARCHAR(50),
+      games_won VARCHAR(50),
+      total_score VARCHAR(50),
+      best_score VARCHAR(50)
+    )`,
     `CREATE TABLE IF NOT EXISTS Word (
       id_word INTEGER PRIMARY KEY,
       label VARCHAR(50),
@@ -27,34 +41,9 @@ async function createSchema() {
     )`,
     `CREATE TABLE IF NOT EXISTS Attempt (
       id_attempt INTEGER PRIMARY KEY,
-      letter VARCHAR(1),
+      letters VARCHAR(1),
       is_correct INTEGER,
       attempt_date DATETIME
-    )`,
-    `CREATE TABLE IF NOT EXISTS Statistic (
-      id_statistic INTEGER PRIMARY KEY,
-      games_played INTEGER,
-      games_won INTEGER,
-      games_lost INTEGER,
-      total_score INTEGER,
-      best_score INTEGER
-    )`,
-    `CREATE TABLE IF NOT EXISTS Ranking (
-      id_ranking VARCHAR(50) PRIMARY KEY,
-      position_ INTEGER,
-      score INTEGER,
-      ranking_date DATE
-    )`,
-    `CREATE TABLE IF NOT EXISTS User_ (
-      id_user INTEGER PRIMARY KEY,
-      username VARCHAR(50),
-      role VARCHAR(50),
-      password VARCHAR(50),
-      creation_date DATE,
-      id_ranking VARCHAR(50),
-      id_statistic INTEGER,
-      FOREIGN KEY(id_ranking) REFERENCES Ranking(id_ranking),
-      FOREIGN KEY(id_statistic) REFERENCES Statistic(id_statistic)
     )`,
     `CREATE TABLE IF NOT EXISTS Game (
       id_game INTEGER PRIMARY KEY,
@@ -63,9 +52,9 @@ async function createSchema() {
       status VARCHAR(50),
       max_errors INTEGER,
       score INTEGER,
-      id_attempt INTEGER,
-      id_word INTEGER,
-      id_user INTEGER,
+      id_attempt INTEGER NOT NULL,
+      id_word INTEGER NOT NULL,
+      id_user INTEGER NOT NULL,
       FOREIGN KEY(id_attempt) REFERENCES Attempt(id_attempt),
       FOREIGN KEY(id_word) REFERENCES Word(id_word),
       FOREIGN KEY(id_user) REFERENCES User_(id_user)
--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -2,13 +2,40 @@ import { Field, Int, ObjectType } from "type-graphql";
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @ObjectType()
-@Entity()
+@Entity({ name: "User_" })
 export class User extends BaseEntity {
   @Field(() => Int)
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn({ name: "id_user" })
+  idUser: number;
 
   @Field()
-  @Column()
-  email: string;
+  @Column({ length: 50 })
+  username: string;
+
+  @Field()
+  @Column({ length: 50 })
+  role: string;
+
+  @Column({ length: 50 })
+  password: string;
+
+  @Field()
+  @Column({ type: "date", name: "creation_date" })
+  creationDate: Date;
+
+  @Field()
+  @Column({ length: 50, name: "games_played" })
+  gamesPlayed: string;
+
+  @Field()
+  @Column({ length: 50, name: "games_won" })
+  gamesWon: string;
+
+  @Field()
+  @Column({ length: 50, name: "total_score" })
+  totalScore: string;
+
+  @Field()
+  @Column({ length: 50, name: "best_score" })
+  bestScore: string;
 }

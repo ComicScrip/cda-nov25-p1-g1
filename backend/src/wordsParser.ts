90dd6e32f0554968f401a04ee0cb7e58e4803551
on4r4p <on4r4p@disrot.org>
2025-12-30 17:11:59 +0100

    playing with letters is fun Dora!

--- backend/src/wordsParser.ts
+++ backend/src/wordsParser.ts
@@ -1,34 +1,31 @@
-import fs from "fs";
+import * as path from "path";
+import * as fs from "fs";
 
+async function lycosGoFetch(stick: string): Promise<string> {
 
+    const response = await fetch(stick)
+    const buff = await response.arrayBuffer();
+    const text = new TextDecoder("latin1").decode(buff)
+    return text;
+}
 
+async function saveToFile(file: string, outputDir: string, text: string) {
+    fs.mkdirSync(outputDir, { recursive: true });
+    fs.writeFileSync(file, text, { encoding: "utf-8" });
+    console.log(`Copy to ${file}`);
+}
 
-const mot = "marteau";
-const url = `http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=${encodeURIComponent(mot)}`;
-const file = `./${mot}.parser.tst`;
 
 
 async function main() {
-
-    const response = await fetch(url);
-    const buff = await response.arrayBuffer();
-    const text = new TextDecoder("utf-8").decode(buff)
-    const lines = text.split("\n");
-
-
-    fs.writeFileSync(file, text, { encoding: "utf-8" });
-
-
-
-    /*     const words: string[] = [];
-    
-        console.log("Lycos Fetched this \n\n\n\n:");
-        for (const line of lines) {
-            console.log(line);
-        } */
-
-
+    const mot = "marteau";
+    const url = `http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=${encodeURIComponent(mot)}`;
+    const outputDir = path.join(__dirname, "parsed_files");
+    const file = path.join(outputDir, `${mot}.parsed`);
+    const response = await lycosGoFetch(url);
+    //await saveToFile(file, outputDir, response);
 
 }
+
 
 main();
